1b8080019f5ffee330244ffcac7a21a4dfa0c0cb
AtulSingh205 <atulsingh07062004@gmail.com>
2025-09-10 21:05:28 +0530

    fullupdate

--- form2/index.js
+++ form2/index.js
@@ -1,6 +1,6 @@
 let container = document.querySelector(".container");
 let form = document.querySelector(".form");
-
+let input = document.querySelectorAll("input")
 
 container.addEventListener("submit", function (e) {
     console.log(e)
@@ -10,11 +10,11 @@ container.addEventListener("submit", function (e) {
     let profile = document.createElement("div")
     profile.classList.add("profile")
     let img = document.createElement("img")
-    img.setAttribute("src", "")
+    img.setAttribute("src", input[0].value)
     let h1 = document.createElement("h1")
-    h1.textContent = ("Hanuman ji")
+    h1.textContent = (input[1].value)
     let h2 = document.createElement("h2")
-    h2.textContent = ("ram bhkt")
+    h2.textContent = (input[2].value)
     card.appendChild(profile)
     profile.appendChild(img)
     card.appendChild(h1)
